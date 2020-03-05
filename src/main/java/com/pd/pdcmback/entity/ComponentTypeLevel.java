9b1d1fcc45479054f9a639ea32687b3b64dd5804
pd <1131391464@qq.com>
2020-03-05 09:32:05 +0800

    添加componentTypeLevel类的属性

--- src/main/java/com/pd/pdcmback/entity/ComponentTypeLevel.java
+++ src/main/java/com/pd/pdcmback/entity/ComponentTypeLevel.java
@@ -1,10 +1,19 @@
 package com.pd.pdcmback.entity;
 
+import lombok.Data;
+
 /**
  * @author pengdong
  * @date 2020/3/5 9:29
  * @function
  * @describe
  */
+@Data
 public class ComponentTypeLevel {
+
+    private Integer id;
+
+    private Integer fatherId;
+
+    private Integer sonId;
 }
