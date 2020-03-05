c5232706adbcd78191c60382c0c5f6873ef3d26a
pd <1131391464@qq.com>
2020-03-05 09:42:52 +0800

    添加ComponentTypeLevelMapper查询方法List<ComponentTypeLevel> selectComponentTypeLevelByFatherId(Integer fatherId);

--- src/main/java/com/pd/pdcmback/mapper/ComponentTypeLevelMapper.java
+++ src/main/java/com/pd/pdcmback/mapper/ComponentTypeLevelMapper.java
@@ -1,10 +1,18 @@
 package com.pd.pdcmback.mapper;
 
+import com.pd.pdcmback.entity.ComponentTypeLevel;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 /**
  * @author pengdong
  * @date 2020/3/5 9:32
  * @function
  * @describe
  */
+@Repository
 public interface ComponentTypeLevelMapper {
+
+    List<ComponentTypeLevel> selectComponentTypeLevelByFatherId(Integer fatherId);
 }
